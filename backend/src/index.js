280d1e25c9b40d350a17d8c7ee475501c6cecf16
Ivan_Samusevich <samusevich.ivan@list.ru>
2025-04-29 11:10:41 +0300

    Add DB

---
+++ backend/src/index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import authRoutes from "../src/routes/auth_route.js";
+import dotenv from "dotenv"
+import { connectDB } from "./lib/db.js";
+
+const app = express();
+
+dotenv.config() 
+const PORT = process.env.PORT
+
+app.use("/api/auth", authRoutes)
+
+app.listen(PORT, () => {
+    console.log("HI Ivan!!!, port = " + PORT);
+    connectDB()
+});
