3f2ffa7e97fa9726a16c0038d517407dfcb6a422
Valentine384 <Valikverenic@gmail.com>
2025-05-08 17:23:01 +0300

    Сделаны страницы входа и регистрации

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -28,11 +28,38 @@ export const useAuthStore = create((set) => ({
         try{
             const res = await axiosInstance.post("/auth/signup", data);
             set({ authUser: res.data })
-            toast.succes("Account created successfully");
+            toast.success("Account created successfully");
         } catch (error) {
             toast.error(error.response.data.message);
         } finally {
             set({ isSigningUp: false })
         }
-    }
+    },
+
+    login: async (data) => {
+        set({ isLoggingIn: true });
+        try {
+          const res = await axiosInstance.post("/auth/login", data);
+          set({ authUser: res.data });
+          toast.success("Logged in successfully");
+    
+          get().connectSocket();
+        } catch (error) {
+          toast.error(error.response.data.message);
+        } finally {
+          set({ isLoggingIn: false });
+        }
+    },    
+
+    logout: async () => {
+        try {
+          await axiosInstance.post("/auth/logout");
+          set({ authUser: null });
+          toast.success("Logged out successfully");
+          get().disconnectSocket();
+        } catch (error) {
+          toast.error(error.response.data.message);
+        }
+    },
+    
 }));
