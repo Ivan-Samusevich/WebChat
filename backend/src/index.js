e95cc0d7e60a712c500b077b548d710394eac5b2
Valentine384 <Valikverenic@gmail.com>
2025-05-08 15:01:14 +0300

    Добавлено перенаправление со страниц

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,22 @@
 import express from "express";
 import authRoutes from "../src/routes/auth_route.js";
 import dotenv from "dotenv"
+import cors from "cors";
+
 import { connectDB } from "./lib/db.js";
 
 const app = express();
 
 dotenv.config() 
 const PORT = process.env.PORT
+
+
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    })
+);
 
 app.use("/api/auth", authRoutes)
 
